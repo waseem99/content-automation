1324a1c6babd1cb1c866a48538ede5d155657a90
waseem99 <waseem99@users.noreply.github.com>
2026-07-05 00:20:46 +0500

    Add rights application package

---
+++ src/application/rights/__init__.py
@@ -0,0 +1,21 @@
+from src.application.rights.gate import RightsGateService
+from src.application.rights.models import (
+    AssetRightsDecision,
+    RightsDecisionOutcome,
+    RightsGateDecision,
+    RightsGatePoint,
+    RightsGateRequest,
+    RightsObligations,
+    RightsPlatform,
+)
+
+__all__ = [
+    "AssetRightsDecision",
+    "RightsDecisionOutcome",
+    "RightsGateDecision",
+    "RightsGatePoint",
+    "RightsGateRequest",
+    "RightsGateService",
+    "RightsObligations",
+    "RightsPlatform",
+]
