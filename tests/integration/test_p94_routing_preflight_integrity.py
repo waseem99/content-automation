3e80efa8eb38eabd4186c6f4578266475ae6172d
waseem99 <waseem99@users.noreply.github.com>
2026-07-21 22:44:14 +0500

    P94 fix routing lifecycle test import

--- tests/integration/test_p94_routing_preflight_integrity.py
+++ tests/integration/test_p94_routing_preflight_integrity.py
@@ -10,7 +10,7 @@ from src.application.routing.models import RoutingPlanRequest
 from src.application.routing.service import RoutingSpendService
 from tests.integration.p89_script_support import p89_database, p89_seeded
 from tests.integration.p91_visual_support import p91_ready
-from tests.integration.p94_routing_lifecycle import activate_policy, mixed_inputs
+from tests.integration.test_p94_routing_lifecycle import activate_policy, mixed_inputs
 from tests.integration.p94_routing_support import p94_ready
 
 
