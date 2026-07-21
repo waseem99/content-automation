575357511ac838f84885dde49a192d07fc9c49ee
waseem99 <waseem99@users.noreply.github.com>
2026-07-21 20:49:40 +0500

    P94 register routing and spend approval routes

--- src/operator_api/runtime_factory.py
+++ src/operator_api/runtime_factory.py
@@ -18,6 +18,7 @@ from src.operator_api.app import create_app
 from src.operator_api.production_workflow_runtime import install_production_workflow_routes
 from src.operator_api.renderers_validated_runtime import install_renderer_routes
 from src.operator_api.review_workspace_runtime import install_review_workspace_routes
+from src.operator_api.routing_runtime import install_routing_routes
 from src.operator_api.runtime_config import OperatorRuntimeSettings, get_operator_runtime_settings
 from src.operator_api.scripts_runtime import install_script_routes
 from src.operator_api.visuals_runtime import install_visual_routes
@@ -44,6 +45,7 @@ def create_configured_app(
     install_visual_routes(app, database=database, auth_settings=auth)
     install_review_workspace_routes(app, database=database, auth_settings=auth)
     install_renderer_routes(app, database=database, auth_settings=auth)
+    install_routing_routes(app, database=database, auth_settings=auth)
     app.state.runtime_settings = settings
 
     @app.get("/runtime/config")
