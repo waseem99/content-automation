b00ae5c4b3aea589b38b30d759d6e25fad98f2ad
waseem99 <waseem99@users.noreply.github.com>
2026-07-21 20:30:43 +0500

    P93 use validated renderer catalogue service

--- src/operator_api/runtime_factory.py
+++ src/operator_api/runtime_factory.py
@@ -16,7 +16,7 @@ from src.operator_api.generation_jobs_runtime import install_generation_job_rout
 from src.operator_api.observability import observability_contract
 from src.operator_api.app import create_app
 from src.operator_api.production_workflow_runtime import install_production_workflow_routes
-from src.operator_api.renderers_runtime import install_renderer_routes
+from src.operator_api.renderers_validated_runtime import install_renderer_routes
 from src.operator_api.review_workspace_runtime import install_review_workspace_routes
 from src.operator_api.runtime_config import OperatorRuntimeSettings, get_operator_runtime_settings
 from src.operator_api.scripts_runtime import install_script_routes
