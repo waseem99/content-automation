bfd57926e89a3a334eed151f65137fb2d067e49b
waseem99 <waseem99@users.noreply.github.com>
2026-07-22 21:26:43 +0500

    Expose local qualification queue

--- workers/acquisition/__init__.py
+++ workers/acquisition/__init__.py
@@ -1,5 +1,6 @@
 """Acquisition worker utilities."""
 
+from .linkedin_queue import QualificationBatchStats, qualify_jsonl, qualify_record
 from .linkedin_qualification import (
     LinkedInOpportunityDecision,
     LinkedInPostSignal,
@@ -8,6 +9,9 @@ from .linkedin_qualification import (
 )
 
 __all__ = [
+    "QualificationBatchStats",
+    "qualify_jsonl",
+    "qualify_record",
     "LinkedInOpportunityDecision",
     "LinkedInPostSignal",
     "OpportunityDisposition",
