f6ffbcaf937368c4dd5f2494c52c8790246b7b28
waseem99 <waseem99@users.noreply.github.com>
2026-07-29 11:37:06 +0500

    P110 install expansion routes in configured runtime

--- src/operator_api/runtime_factory.py
+++ src/operator_api/runtime_factory.py
@@ -29,6 +29,7 @@ from src.operator_api.observability import observability_contract
 from src.operator_api.app import create_app
 from src.operator_api.operations_middleware import OperationsSafetyMiddleware
 from src.operator_api.operations_validated_runtime import install_operations_routes
+from src.operator_api.p110_runtime import install_p110_routes
 from src.operator_api.performance_runtime import install_performance_routes
 from src.operator_api.production_workflow_runtime import install_production_workflow_routes
 from src.operator_api.releases_validated_runtime import install_release_routes
@@ -66,6 +67,7 @@ def create_configured_app(
     install_generation_job_routes(app, database=database, auth_settings=auth)
     install_concept_routes(app, database=database, auth_settings=auth)
     install_script_routes(app, database=database, auth_settings=auth)
+    install_p110_routes(app, database=database, auth_settings=auth)
     install_audio_routes(app, database=database, auth_settings=auth)
     install_visual_routes(app, database=database, auth_settings=auth)
     install_review_workspace_routes(app, database=database, auth_settings=auth)
