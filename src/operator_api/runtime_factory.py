c25582b709d5a53a3fe3b43e753c132573d82d67
waseem99 <waseem99@users.noreply.github.com>
2026-07-29 17:14:29 +0500

    P114 register local video API

--- src/operator_api/runtime_factory.py
+++ src/operator_api/runtime_factory.py
@@ -25,6 +25,7 @@ from src.operator_api.brand_profiles_runtime import install_brand_profile_routes
 from src.operator_api.concepts_runtime import install_concept_routes
 from src.operator_api.delivery_runtime import install_delivery_routes
 from src.operator_api.generation_jobs_runtime import install_generation_job_routes
+from src.operator_api.local_video_runtime import install_local_video_routes
 from src.operator_api.observability import observability_contract
 from src.operator_api.app import create_app
 from src.operator_api.operations_middleware import OperationsSafetyMiddleware
@@ -77,6 +78,7 @@ def create_configured_app(
     install_renderer_routes(app, database=database, auth_settings=auth)
     install_routing_routes(app, database=database, auth_settings=auth)
     install_routing_workspace_routes(app, database=database, auth_settings=auth)
+    install_local_video_routes(app, database=database, auth_settings=auth)
     install_release_routes(app, database=database, auth_settings=auth)
     install_delivery_routes(app, database=database, auth_settings=auth)
     install_performance_routes(app, database=database, auth_settings=auth)
