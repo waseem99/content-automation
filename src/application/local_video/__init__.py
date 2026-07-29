04147ed36fb06db99a8264cbe38d0ae88dd5b346
waseem99 <waseem99@users.noreply.github.com>
2026-07-29 16:58:03 +0500

    P114 export manifest validation

--- src/application/local_video/__init__.py
+++ src/application/local_video/__init__.py
@@ -1,13 +1,21 @@
 from src.application.local_video.comfy import ComfyUIClient, ComfyUIError
+from src.application.local_video.manifest import (
+    ActivatedWorkflow,
+    LocalVideoManifestError,
+    load_and_verify_manifest,
+)
 from src.application.local_video.models import LocalVideoRequest, LocalVideoResult
 from src.application.local_video.workflow import LocalVideoWorkflowError, VerifiedWorkflow, verify_workflow
 
 __all__ = [
+    "ActivatedWorkflow",
     "ComfyUIClient",
     "ComfyUIError",
+    "LocalVideoManifestError",
     "LocalVideoRequest",
     "LocalVideoResult",
     "LocalVideoWorkflowError",
     "VerifiedWorkflow",
+    "load_and_verify_manifest",
     "verify_workflow",
 ]
