b2566c54577ad398ff55b4c2548e9d341d426c4b
waseem99 <waseem99@users.noreply.github.com>
2026-08-01 20:29:40 +0500

    P120 install typed autopilot run query

--- src/application/pre_generation/__init__.py
+++ src/application/pre_generation/__init__.py
@@ -2,5 +2,10 @@ from src.application.pre_generation.service import (
     PreGenerationError,
     PreGenerationService,
 )
+from src.application.pre_generation.runtime_patch import install_pre_generation_runtime_patch
+
+
+install_pre_generation_runtime_patch()
+
 
 __all__ = ["PreGenerationError", "PreGenerationService"]
