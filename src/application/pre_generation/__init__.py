b206ffd2a83e4deab8dec78312878205b57c73df
waseem99 <waseem99@users.noreply.github.com>
2026-08-01 21:23:23 +0500

    P120 install final bounded runtime corrections

--- src/application/pre_generation/__init__.py
+++ src/application/pre_generation/__init__.py
@@ -3,9 +3,11 @@ from src.application.pre_generation.service import (
     PreGenerationService,
 )
 from src.application.pre_generation.runtime_patch import install_pre_generation_runtime_patch
+from src.application.pre_generation.final_runtime_patch import install_final_pre_generation_runtime_patch
 
 
 install_pre_generation_runtime_patch()
+install_final_pre_generation_runtime_patch()
 
 
 __all__ = ["PreGenerationError", "PreGenerationService"]
