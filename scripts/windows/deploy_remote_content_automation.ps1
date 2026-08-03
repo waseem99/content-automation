711a035fe1fb449e10569c64f948bfbb985bb313
waseem99 <waseem99@users.noreply.github.com>
2026-08-03 10:12:52 +0500

    preserve canonical migration head in remote deployment

--- scripts/windows/deploy_remote_content_automation.ps1
+++ scripts/windows/deploy_remote_content_automation.ps1
@@ -49,7 +49,7 @@ function Sync-P126Environment([string]$Path) {
   $values["HYBRID_PAID_EXECUTION_ENABLED"] = "false"
   $values["HYBRID_PUBLIC_PUBLISHING_ENABLED"] = "false"
   $values["LOCAL_SCRIPT_TIMEOUT_SECONDS"] = "120"
-  $values["OPS_MIGRATION_HEAD"] = "0103_p126_hybrid_scene_routing.sql"
+  $values["OPS_MIGRATION_HEAD"] = "0108_p131_staged_acceptance_closeout.sql"
   $values["OPS_MAX_REQUEST_BODY_BYTES"] = "67108864"
   [IO.File]::WriteAllLines(
     $Path,
