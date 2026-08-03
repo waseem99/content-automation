e9ab46939b0d672951ff46a25ca824ab940a5b52
waseem99 <waseem99@users.noreply.github.com>
2026-08-03 22:28:05 +0500

    P135 keep campaign storage package initialization minimal

--- src/application/campaign_storage/__init__.py
+++ src/application/campaign_storage/__init__.py
@@ -2,12 +2,5 @@ from src.application.campaign_storage.service import (
     CampaignStorageError,
     CampaignStorageService,
 )
-from src.application.campaign_storage.google_drive_transport_patch import (
-    install_google_drive_transport_patch,
-)
-
-
-install_google_drive_transport_patch()
-
 
 __all__ = ["CampaignStorageError", "CampaignStorageService"]
